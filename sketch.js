f6bf2ccae62af2d2e62082be22fc96516642a7a9
Hamburami <harrisonpena1@gmail.com>
2025-07-12 15:36:18 +0200

    Noise

--- sketch.js
+++ sketch.js
@@ -1,19 +1,17 @@
 let tiles = [];
+let noiseScale = 0.01
+let resolution = 300
+let canvasSize = 800
 
 function setup() {
-  createCanvas(500, 500);
-  // optional: frameRate(10);
+  createCanvas(canvasSize, canvasSize);
+  noiseDetail(12, 0.5);
 
-  for (let x = 0; x <= 100; x++) {
+  for (let x = 0; x <= resolution; x++) {
     tiles[x] = [];
-    for (let y = 0; y <= 100; y++) {
+    for (let y = 0; y <= resolution; y++) {
       // store the coordinates in each Tile
-      tiles[x][y] = new Tile(
-        x,
-        y,
-        (cos((PI * x) / 50) + 1) / 2,
-        (cos((PI * y) / 50) + 1) / 2
-      );
+      tiles[x][y] = new Tile(x,y, noise(noiseScale*x, noiseScale*y), 0);
     }
   }
 }
@@ -22,12 +20,14 @@ function draw() {
   background(220);
   noStroke();
 
-  for (let x = 0; x <= 100; x++) {
-    for (let y = 0; y <= 100; y++) {
-      tiles[x][y].show(5);
+  for (let x = 0; x <= resolution; x++) {
+    for (let y = 0; y <= resolution; y++) {
+      tiles[x][y].show(canvasSize/resolution);
     }
   }
 }
+
+let cutOff = 0.5
 
 class Tile {
   constructor(x, y, h, p) {
@@ -38,9 +38,9 @@ class Tile {
   }
 
   show(tileSize) {
-    if (this.height > 0.6) {
+    if (this.height > cutOff) {
       fill(color("green"));
-    } else if (this.height > 0.5) {
+    } else if (this.height > cutOff -0.05) {
       fill(color("tan"));
     } else {
       fill(color("blue"));
